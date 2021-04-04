56175f69a7bce55d9c7bcc483b4d146ab87d15e7
Josh White <jcrwhite@users.noreply.github.com>
2021-04-03 20:18:06 -0400

    remove unused utils

--- src/utils/isUndefined.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const isUndefined = (v: any): boolean => v === undefined;
--- src/utils/toNumber.ts
+++
@@ -1,9 +0,0 @@
-export const toNumber = (v: number | string | boolean | undefined): number => {
-  if (typeof v === 'number') {
-    return v;
-  }
-  if (!v) {
-    return 0;
-  }
-  return Number(v);
-};
